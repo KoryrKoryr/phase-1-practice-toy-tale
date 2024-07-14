b7ffb0a9bd9f45ccbce5cb30ed653982a01dbc63
Denis Korir <denohrkoryr@gmail.com>
2024-07-14 17:30:32 +0300

    Toy tale practice update

--- src/index.js
+++ src/index.js
@@ -38,8 +38,9 @@ document.addEventListener("DOMContentLoaded", () => {
         Accept: "application/json",
       },
       body: JSON.stringify({
-        name: toyName,
-        image: toyImage,
+        name: "Jessie",
+        image:
+          "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
         likes: 0,
       }),
     })
